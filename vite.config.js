8b98b8c91b2f1f1b8a903873293b276608e32c4e
crayolito <josalejandroapp2017@gmail.com>
2022-10-26 13:12:44 -0400

    ola

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://crayolito.github.io/React-Rick-Morty.git/",
+  base: "https://crayolito.github.io/React-Rick-Morty/",
   plugins: [react()]
 })
